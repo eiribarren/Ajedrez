740fbea56d354938dd39b6f45a443f058d84b843
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-05-06 01:38:03 +0200

    Caballo.java
    He codificado los movimientos posibles de la pieza Caballo.

--- AjedrezG/src/Juego/Caballo.java
+++ AjedrezG/src/Juego/Caballo.java
@@ -13,9 +13,33 @@ public class Caballo extends Pieza{
 		this.setIcon(new javax.swing.ImageIcon(getClass().getResource(urlImagen)));
 		this.setPreferredSize(new Dimension(100,100));
 	}
+	//Columna=X Fila=Y
 	public int[][] getMovimientos(){
-		int[][] movimientos = new int[2][2];
-		return movimientos;
+		int[][] movimientos = new int[8][2];
+			movimientos[0][0]=getFila()+2;//moviment primer fila
+			movimientos[0][1]=getColumna()+1;//moviment primer columna
+			
+			movimientos[1][0]=getFila()+2; //moviment segon fila
+			movimientos[1][1]=getColumna()-1; //moviment segon columna
+			
+			movimientos[2][0]=getFila()+1;//moviment primer fila
+			movimientos[2][1]=getColumna()+2;//moviment primer columna
+			
+			movimientos[3][0]=getFila()-1; //moviment segon fila
+			movimientos[3][1]=getColumna()+2; //moviment segon columna
+			
+			movimientos[4][0]=getFila()-2;//moviment primer fila
+			movimientos[4][1]=getColumna()-1;//moviment primer columna
+			
+			movimientos[5][0]=getFila()+1; //moviment segon fila
+			movimientos[5][1]=getColumna()-2; //moviment segon columna
+			
+			movimientos[6][0]=getFila()-1; //moviment segon fila
+			movimientos[6][1]=getColumna()-2; //moviment segon columna
+			
+			movimientos[7][0]=getFila()+1; //moviment segon fila
+			movimientos[7][1]=getColumna()-2; //moviment segon columna
+			return movimientos;
 	}
 }
 
