44da4a10b1b2ef4df6053a890faa642e831d3b4e
jan_6 <jan_6@DESKTOP-04MRKHC>
2018-05-06 21:12:40 +0200

    Hey chicos! He metido al rey en el programa, cuidado con el rey negro xd

--- AjedrezG/src/Juego/Rey.java
+++ AjedrezG/src/Juego/Rey.java
@@ -3,7 +3,11 @@ import java.awt.Dimension;
 import java.awt.Color;
 
 public class Rey extends Pieza{
-	public Rey(Color color) {
+	public Rey(Color color, int fila, int columna) {
+		this.identificador = "Rey";
+		this.columna = columna;
+		this.fila = fila;
+		this.color = color;
 		this.setVisible(true);
 		if ( color == Color.BLACK ) {
 			this.urlImagen = "/Juego/ReyNegro.png";
@@ -14,7 +18,30 @@ public class Rey extends Pieza{
 		this.setPreferredSize(new Dimension(100,100));
 	}
 	public int[][] getMovimientos(){
-		int[][] movimientos = new int[2][2];
+		int[][] movimientos = new int[8][2];
+			movimientos[0][0]=getFila()+1;
+			movimientos[0][1]=getColumna();
+			
+			movimientos[1][0]=getFila()-1;
+			movimientos[1][1]=getColumna();
+		
+			movimientos[2][0]=getFila();
+			movimientos[2][1]=getColumna()+1;
+			
+			movimientos[3][0]=getFila();
+			movimientos[3][1]=getColumna()-1;
+			
+			movimientos[4][0]=getFila()+1;
+			movimientos[4][1]=getColumna()+1;
+			
+			movimientos[5][0]=getFila()+1;
+			movimientos[5][1]=getColumna()-1;
+			
+			movimientos[6][0]=getFila()-1;
+			movimientos[6][1]=getColumna()+1;
+			
+			movimientos[7][0]=getFila()-1;
+			movimientos[7][1]=getColumna()-1;
 		return movimientos;
 	}
 }
