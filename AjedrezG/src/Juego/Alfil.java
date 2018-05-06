65ca6a59c13fa86d195cbf4c8a0d1f742adab8d3
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-05-06 02:25:41 +0200

    Alfil.java
    He codificado los movimientos posibles y su constructor de la pieza Alfil.

--- AjedrezG/src/Juego/Alfil.java
+++ AjedrezG/src/Juego/Alfil.java
@@ -3,7 +3,11 @@ import java.awt.Dimension;
 import java.awt.Color;
 
 public class Alfil extends Pieza{
-	public Alfil(Color color) {
+	public Alfil(Color color, int fila, int columna) {
+		this.identificador = "Alfil";
+		this.columna = columna;
+		this.fila = fila;
+		this.color = color;
 		this.setVisible(true);
 		if ( color == Color.BLACK ) {
 			this.urlImagen = "/Juego/AlfilNegro.png";
@@ -12,9 +16,26 @@ public class Alfil extends Pieza{
 		}
 		this.setIcon(new javax.swing.ImageIcon(getClass().getResource(urlImagen)));
 		this.setPreferredSize(new Dimension(100,100));
+		start();
 	}
 	public int[][] getMovimientos(){
-		int[][] movimientos = new int[2][2];
+		int[][] movimientos = new int[28][2];
+		for(int i=1;i<8;i++){
+			movimientos[i-1][0]=getFila()+(1*i);//moviment primer fila
+			movimientos[i-1][1]=getColumna()+(1*i);//moviment primer columna
+		}
+		for(int i=1;i<8;i++){
+			movimientos[i+6][0]=getFila()-(1*i);//moviment primer fila
+			movimientos[i+6][1]=getColumna()+(1*i);//moviment primer columna
+		}
+		for(int i=1;i<8;i++){
+			movimientos[i+13][0]=getFila()+(1*i);//moviment primer fila
+			movimientos[i+13][1]=getColumna()-(1*i);//moviment primer columna
+		}
+		for(int i=1;i<8;i++){
+			movimientos[i+20][0]=getFila()-(1*i);//moviment primer fila
+			movimientos[i+20][1]=getColumna()-(1*i);//moviment primer columna
+		}
 		return movimientos;
 	}
 }
