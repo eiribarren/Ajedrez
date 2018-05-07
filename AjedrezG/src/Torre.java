fc7948a94699c1de7bdee8545bb597258300cb10
eric10499 <34514368+eric10499@users.noreply.github.com>
2018-05-07 18:39:33 +0200

    Torre.java
    He codificado la clase Torre.

--- AjedrezG/src/Torre.java
+++ AjedrezG/src/Torre.java
@@ -3,7 +3,11 @@ import java.awt.Dimension;
 import java.awt.Color;
 
 public class Torre extends Pieza{
-	public Torre(Color color) {
+	public Torre(Color color, int fila, int columna) {
+		this.identificador = "Torre";
+		this.columna = columna;
+		this.fila = fila;
+		this.color = color;
 		this.setVisible(true);
 		if ( color == Color.BLACK ) {
 			this.urlImagen = "/TorreNegra.png";
@@ -11,10 +15,32 @@ public class Torre extends Pieza{
 			this.urlImagen = "/TorreBlanca.png";
 		}
 		this.setIcon(new javax.swing.ImageIcon(getClass().getResource(urlImagen)));
-		this.setPreferredSize(new Dimension(50,50));
+		this.setPreferredSize(new Dimension(100,100));
+		start();
 	}
 	public int[][] getMovimientos(){
-		int[][] movimientos = new int[2][2];
+		int[][] movimientos = new int[28][2];
+		 
+		
+		for(int i=1;i<8;i++){
+	   		 movimientos[i-1][0] = getFila()+(1*i);
+	   		 movimientos[i-1][1] = getColumna();
+	   	 }
+	   	 
+	   	 for(int i=1;i<8;i++){
+	   		 movimientos[i+6][0] = getFila()-(1*i);
+	   		 movimientos[i+6][1] = getColumna();
+	   	 }
+	   	 
+	   	 for(int i=1;i<8;i++){
+	   		 movimientos[i+13][0] = getFila();
+	   		 movimientos[i+13][1] = getColumna()-(1*i);
+	   	 }
+	   	 
+	   	 for(int i=1;i<8;i++){
+	   		 movimientos[i+20][0] = getFila();
+	   		 movimientos[i+20][1] = getColumna()+(1*i);
+	   	 }
 		return movimientos;
 	}
 }
